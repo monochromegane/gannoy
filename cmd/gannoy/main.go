30c96f32138ce2e9c17b2bf512572c9ba4ef13dc
monochromegane <dev.kuro.obi@gmail.com>
2017-05-04 21:55:32 +0900

    Organized gannoy command options.

--- cmd/gannoy/main.go
+++ cmd/gannoy/main.go
@@ -1,38 +1,49 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"os"
 
+	flags "github.com/jessevdk/go-flags"
 	"github.com/monochromegane/gannoy"
 )
 
-var (
-	path     string
-	database string
-	dim      int
-	tree     int
-	K        int
-)
+type CreateCommand struct {
+	Dim  int    `short:"d" long:"dim" default:"2" description:"Specify size of feature dimention."`
+	Tree int    `short:"t" long:"tree" default:"1" description:"Specify size of index tree."`
+	K    int    `short:"K" long:"K" default:"50" description:"Specify max node size in a bucket node."`
+	Path string `short:"p" long:"path" default:"." description:"Build meta file into this directory."`
+}
 
-func init() {
-	flag.StringVar(&path, "p", ".", "Database path.")
-	flag.StringVar(&database, "d", "", "Database name.")
-	flag.IntVar(&dim, "dim", 2, "Dimention.")
-	flag.IntVar(&tree, "tree", 1, "Tree.")
-	flag.IntVar(&K, "K", 50, "K.")
-	flag.Parse()
+var createCommand CreateCommand
+
+func (c *CreateCommand) Execute(args []string) error {
+	if len(args) != 1 {
+		return fmt.Errorf("database name not specified.")
+	}
+	if c.K < 4 {
+		return fmt.Errorf("K must be at least 4 or more, but %d.", c.K)
+	}
+	err := gannoy.CreateMeta(c.Path, args[0], c.Tree, c.Dim, c.K)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func (c *CreateCommand) Usage() string {
+	return "[create-OPTIONS] DATABASE"
 }
 
 func main() {
-	if K < 4 {
-		fmt.Fprintf(os.Stderr, "K must be at least 4 or more, but %d.\n", K)
-		os.Exit(1)
-	}
-	err := gannoy.CreateMeta(path, database, tree, dim, K)
+	parser := flags.NewParser(nil, flags.Default)
+	parser.Name = "gannoy"
+
+	parser.AddCommand("create",
+		"Create database",
+		"The create command creates a meta file for the database.",
+		&createCommand)
+	_, err := parser.Parse()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "%v\n", err)
 		os.Exit(1)
 	}
 }
