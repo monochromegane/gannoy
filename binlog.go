2aa7a7a5dc0649aabb8593a3010f6966df06bcb8
monochromegane <dev.kuro.obi@gmail.com>
2017-12-07 17:03:23 +0900

    Add endpoint to be updated from binlog.

--- binlog.go
+++ binlog.go
@@ -22,13 +22,17 @@ func (b *BinLog) Open() error {
 		return err
 	}
 	_, err = db.Exec(
-		`create table if not exists "features" ("key" integer primary key, "action" integer, "features" blob)`,
+		`create table if not exists "features" (
+			"key" integer primary key,
+			"action" integer,
+			"features" blob,
+			"updated_at" timestamp default (datetime('now','localtime')))`,
 	)
 	if err != nil {
 		return err
 	}
 	stms, err := db.Prepare(
-		`insert or replace into features values (?, ?, ?)`,
+		`insert or replace into features (key, action, features) values (?, ?, ?)`,
 	)
 	if err != nil {
 		return err
@@ -48,5 +52,14 @@ func (b BinLog) Close() error {
 
 func (b BinLog) Add(key, action int, features []byte) error {
 	_, err := b.stms.Exec(key, action, features)
+	return err
+}
+
+func (b BinLog) Get(current string) (*sql.Rows, error) {
+	return b.db.Query(`select key, action, features from features where updated_at <= ?`, current)
+}
+
+func (b BinLog) Clear(current string) error {
+	_, err := b.db.Exec(`delete from features where updated_at <= ?`, current)
 	return err
 }
--- ngt.go
+++ ngt.go
@@ -2,8 +2,12 @@ package gannoy
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"os"
+	"path"
 	"path/filepath"
 	"time"
 
@@ -114,6 +118,175 @@ func (idx *NGTIndex) GetAllNns(v []float64, n int, epsilon float32) ([]int, erro
 
 func (idx *NGTIndex) UpdateBinLog(key, action int, features []byte) error {
 	return idx.bin.Add(key, action, features)
+}
+
+type Feature struct {
+	W []float64 `json:"features"`
+}
+
+func (idx *NGTIndex) ApplyBinLog() error {
+	tmp, err := ioutil.TempDir("", "gannoy")
+	if err != nil {
+		return err
+	}
+	defer os.RemoveAll(tmp)
+
+	// Copy db and map
+	tmpdb, err := idx.copyDatabase(tmp)
+	if err != nil {
+		return err
+	}
+	tmpmap, err := idx.copyMap(tmp)
+	if err != nil {
+		return err
+	}
+
+	// Open as new NGTIndex
+	index, err := ngt.OpenIndex(tmpdb)
+	if err != nil {
+		return err
+	}
+	pair, err := newPair(tmpmap)
+	if err != nil {
+		return err
+	}
+
+	// Get current time
+	current := time.Now().Format("2006-01-02 03:04:05")
+
+	// Select from binlog where current time
+	rows, err := idx.bin.Get(current)
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+
+	// Apply binlog
+	for rows.Next() {
+		var key int
+		var action int
+		var features []byte
+
+		err := rows.Scan(&key, &action, &features)
+		if err != nil {
+			return err
+		}
+		switch action {
+		case DELETE:
+			if id, ok := pair.idFromKey(uint(key)); ok {
+				err := index.RemoveIndex(id.(uint))
+				if err != nil {
+					return err
+				}
+				pair.removeByKey(uint(key))
+			}
+		case UPDATE:
+			var f Feature
+			err = json.Unmarshal(features, &f)
+			if err != nil {
+				return err
+			}
+			newId, err := index.InsertIndex(f.W)
+			if err != nil {
+				return err
+			}
+			pair.addPair(uint(key), newId)
+		}
+	}
+	err = pair.save()
+	if err != nil {
+		return err
+	}
+	err = index.CreateIndex(idx.thread)
+	if err != nil {
+		return err
+	}
+	err = index.SaveIndex(tmpdb)
+	if err != nil {
+		return err
+	}
+
+	// Defer: delete old binlog (timestamp < current time)
+	err = idx.bin.Clear(current)
+	if err != nil {
+		return err
+	}
+
+	// Overwrite
+	err = os.Rename(tmpmap, idx.pair.file)
+	if err != nil {
+		return err
+	}
+	files, err := ioutil.ReadDir(tmpdb)
+	if err != nil {
+		return err
+	}
+	for _, f := range files {
+		err = os.Rename(filepath.Join(tmpdb, f.Name()), filepath.Join(idx.database, f.Name()))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (idx *NGTIndex) Save() error {
+	err := idx.pair.save()
+	if err != nil {
+		return err
+	}
+	return idx.index.SaveIndex(idx.database)
+}
+
+func (idx NGTIndex) copyDatabase(tmp string) (string, error) {
+	path := filepath.Join(tmp, path.Base(idx.database))
+	err := os.Mkdir(path, 0755)
+	if err != nil {
+		return "", err
+	}
+
+	files, err := ioutil.ReadDir(idx.database)
+	if err != nil {
+		return "", err
+	}
+	for _, f := range files {
+		src, err := os.Open(filepath.Join(idx.database, f.Name()))
+		if err != nil {
+			return "", err
+		}
+
+		dst, err := os.Create(filepath.Join(path, f.Name()))
+		if err != nil {
+			return "", err
+		}
+		_, err = io.Copy(dst, src)
+		if err != nil {
+			return "", err
+		}
+
+		src.Close()
+		dst.Close()
+	}
+
+	return path, nil
+}
+
+func (idx NGTIndex) copyMap(tmp string) (string, error) {
+	src, err := os.Open(idx.pair.file)
+	if err != nil {
+		return "", err
+	}
+	defer src.Close()
+
+	path := filepath.Join(tmp, path.Base(idx.pair.file))
+	dst, err := os.Create(path)
+	if err != nil {
+		return "", err
+	}
+	defer dst.Close()
+
+	_, err = io.Copy(dst, src)
+	return path, err
 }
 
 func (idx *NGTIndex) searchWithTimeout(resultCh chan searchResult) searchResult {
